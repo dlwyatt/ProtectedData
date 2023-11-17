f2df8f28b0b8c453f5bf54c6940eb5308af2612e
Raimund Andree <r.andree@live.com>
2023-11-17 17:17:38 +0100

    Accessing Security.Cryptography.RSACng

--- ProtectedData.psm1
+++ ProtectedData.psm1
@@ -1,5 +1,8 @@
 $path = Split-Path $MyInvocation.MyCommand.Path
 
+#Access a type in the System.Security.Cryptography namespace to load the assembly into the PowerShell runspace.
+[void][Security.Cryptography.RSACng].Module
+
 Add-Type -Path $path\Security.Cryptography.dll -ErrorAction Stop
 
 . $path\PinnedArray.ps1
